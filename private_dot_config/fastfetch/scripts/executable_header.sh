bac701331d745ead446f5539095137406a66d019
Heather <148720088+Hasseroeder@users.noreply.github.com>
2026-05-21 23:28:11 +0200

    fix: this didn't work before, no idea why I pushed

--- private_dot_config/fastfetch/scripts/executable_header.sh
+++ private_dot_config/fastfetch/scripts/executable_header.sh
@@ -5,11 +5,10 @@
 # $3 is full length
 
 header="┌"
-header+=$(printf '─%.0s' $(seq 1 $1))
-
-header+=" "$2" "
+header="$header$(printf '─%.0s' $(seq 1 $1))"
+header="$header $2 "
 
 right_padding=$(expr "$3 - ${#header} - 1")
-header+=$(printf '─%.0s' {1..$right_padding})
-header+="┐"
-printf "\\u001b[90m$header\n"
+header="$header$(printf '─%.0s' {1..$right_padding})"
+header="$header┐"
+printf "\e[38;2;153;170;187m$header\n"
