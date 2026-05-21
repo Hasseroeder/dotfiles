bc57f6226523ccf8e4f8932853a7ab05dd27a016
Heather <148720088+Hasseroeder@users.noreply.github.com>
2026-05-21 23:44:45 +0200

    fix: fixed colors

--- private_dot_config/fastfetch/scripts/executable_header.sh
+++ private_dot_config/fastfetch/scripts/executable_header.sh
@@ -8,7 +8,7 @@ header="┌"
 header="$header$(printf '─%.0s' $(seq 1 $1))"
 header="$header $2 "
 
-right_padding=$(expr "$3 - ${#header} - 1")
-header="$header$(printf '─%.0s' {1..$right_padding})"
+right_padding=$(expr $3 - ${#header} - 1)
+header="$header$(printf '─%.0s' {$(seq 1 $right_padding)})"
 header="$header┐"
-printf "\e[38;2;153;170;187m$header\n"
+printf "\e[0;90m$header\n"
